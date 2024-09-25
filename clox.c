00527f65d78940ebc25b996dba47be433959123e
amageton23 <91231816+amageton23@users.noreply.github.com>
2024-09-25 12:25:17 +1000

    exercise update

--- clox.c
+++ clox.c
@@ -1,4 +1,7 @@
-using name
+#include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main(){
 
@@ -7,18 +10,42 @@ int main(){
 
 
 struct Node {    
-    int data;
+    char data[100];
     struct Node* prev;
 
     struct Node* next;
 };
 
-struct Node *createNode(int new_data){
+struct linkedList{
+    struct Node* firstNode;
+    struct Node* lastNode;
+};
+
+struct Node *createNode(char new_data[100]){
     struct Node* new_node = (struct Node *)
     malloc(sizeof(struct Node));
-    new_node -> data = new_data;
+    struct linkedList* linked = (struct linkedList *)
+    malloc(sizeof(struct linkedList));
+    new_node -> data[100] = new_data[100];
     new_node -> next = NULL;
     new_node -> prev = NULL;
+    linked ->firstNode = new_node;
+    linked ->lastNode = new_node;
+    
     return new_node;
+}
+void viewNodes( struct linkedList* linked){
+   
+   
+    struct Node* curr = (*linked).firstNode;
+    while(curr != NULL){
+        printf("&d",curr -> data[100]);
+
+        curr = curr.next;
+    }
+}
+
+struct Node *insertNode(int new_data){
+    struct 
 }
 
